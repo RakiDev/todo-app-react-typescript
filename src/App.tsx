78228e50460426ba060938617f3dbe28bd206d10
Raki <killer.vp.mic@gmail.com>
2023-01-22 11:52:41 -0300

    delete useless hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
 import './App.css';
 import Form from './Form'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+  return (<>
     <div className="App">
       <Form />
     </div>
+    <br />
+    </>
   )
 }
 
