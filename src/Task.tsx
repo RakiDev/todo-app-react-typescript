87711896e3d802db7d1fb0bbd8e7d6889bf7b7cc
Raki <killer.vp.mic@gmail.com>
2023-01-24 23:44:04 -0300

    auto-sizing textarea task inputs

--- src/Task.tsx
+++ src/Task.tsx
@@ -12,20 +12,27 @@ interface TaskPropsInterface {
 const Task: FC<TaskPropsInterface> = ({ id, value, onDelete, modTaskValue }) => {
     const [fieldValue, setFieldValue] = useState<string>(value);
     const [isDisabled, setIsDisabled] = useState<boolean>(true);
+    const [minHeight, setMinHeight] = useState<number>(50);
 
-    function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>): void {
+        const paddingTop = parseInt(getComputedStyle(e.target).getPropertyValue('padding-top'));
+        const paddingBottom = parseInt(getComputedStyle(e.target).getPropertyValue('padding-bottom'));
+        const verticalPadding = paddingTop + paddingBottom;
+
+        e.target.style.height = Math.max(e.target.scrollHeight - verticalPadding, minHeight) + 'px';
         const newValue = e.target.value;
         setFieldValue(newValue);
     }
 
     return (
         <li id={`${id}`} className="task_item_li">
-            <input type="text"
+            <textarea
                 value={fieldValue} 
                 onChange={handleChange} 
                 disabled={isDisabled}
                 spellCheck={false}
                 id="focus-input"
+                style={{ minHeight: minHeight }}
             />
             <div className="task_buttons_container">
                 <button onClick={() => {modTaskValue(id, fieldValue); setIsDisabled(true)}} className="taskbutton onpressbutton onhoverbutton">
